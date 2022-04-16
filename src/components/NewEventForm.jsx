9f721e9bd10d4351239b50b8c1cbb5803c27b1c8
KhoiNgLe <nguy0572@algonquinlive.com>
2022-04-15 21:18:12 -0400

    try useRef

--- src/components/NewEventForm.jsx
+++ src/components/NewEventForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./NewEventForm.css";
 const NewEventForm = ({ addEvent }) => {
   const [title, setTitle] = useState("");
